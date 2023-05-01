69a7f9432aef70279eb13b3a158e9d34737efd10
Jakub <jakubmolek@protonmail.ch>
2023-05-02 00:03:42 +0200

    Update db context interface with new aggregates

--- clar2/src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ clar2/src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,5 +1,7 @@
-﻿using clar2.Domain.ToDoItems;
+﻿using clar2.Domain.Notes;
+using clar2.Domain.ToDoItems;
 using clar2.Domain.TodoLists;
+using clar2.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace clar2.Application.Common.Interfaces;
@@ -8,6 +10,10 @@ public interface IApplicationDbContext {
   DbSet<TodoList> TodoLists { get; }
 
   DbSet<TodoItem> TodoItems { get; }
+  
+  DbSet<Note> Notes { get; }
+  
+  DbSet<User> Users { get; }
 
   Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
