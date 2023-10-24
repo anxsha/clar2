14f9c28e2a40d34254440ad1049f3f90ce797916
Jakub <jakubmolek@protonmail.ch>
2023-10-24 12:00:46 +0200

    Add query for fetching archived notes

---
+++ clar2/src/Application/Notes/Queries/GetArchivedAuthoredNotesWithPagination/GetArchivedAuthoredNotesWithPaginationQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace clar2.Application.Notes.Queries.GetArchivedAuthoredNotesWithPagination;
+
+public class
+  GetArchivedAuthoredNotesWithPaginationQueryValidator : AbstractValidator<
+    GetArchivedAuthoredNotesWithPaginationQuery> {
+  public GetArchivedAuthoredNotesWithPaginationQueryValidator() {
+    RuleFor(x => x.PageNumber)
+      .GreaterThanOrEqualTo(1).WithMessage("PageNumber at least greater than or equal to 1.");
+
+    RuleFor(x => x.PageSize)
+      .GreaterThanOrEqualTo(1).WithMessage("PageSize at least greater than or equal to 1.");
+  }
+}
